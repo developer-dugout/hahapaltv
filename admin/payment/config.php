082604445738f325e6a08a778a5e917dc8c01698
Developer <59611518+developer-dugout@users.noreply.github.com>
2020-12-05 02:06:13 +0500

    initial commit

---
+++ admin/payment/config.php
@@ -0,0 +1,13 @@
+<?php 
+// Product Details 
+ 
+// Stripe API configuration  
+define('STRIPE_API_KEY', 'sk_test_wPoX5Ui5P8XjaFu8OoB13w9L00Z5oFPl8y'); 
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_tcPXYHJrY673FLccNvhrjRM300VD2kYfSk'); 
+  
+// Database configuration  
+define('DB_HOST', 'localhost'); 
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', ''); 
+define('DB_NAME', 'stream');
+?>
